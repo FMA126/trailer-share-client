2a9c0cb1920fdbbf05d65d1175a6fa3634e214a5
FMA126 <mhblanks@gmail.com>
2019-06-10 17:16:28 -0400

    Changed curl scripts to create and update

--- curl-scripts/trailers/create.sh
+++ curl-scripts/trailers/create.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 curl "https://trailer-share.herokuapp.com/trailers/" \
-  --include \
-  --request POST \
-  --header "Content-Type: application/json" \
-  --header "Authorization: Token token=${TOKEN}" \
-  --data '{
-    "trailer": {
-      "make": "'"${MAKE}"'",
-      "model": "'"${MODEL}"'"
-    }
-  }'
+--include \
+--request POST \
+--header "Content-Type: application/json" \
+--header "Authorization: Token token=${TOKEN}" \
+--data '{
+  "trailer": {
+    "make": "'"${MAKE}"'",
+    "model": "'"${MODEL}"'",
+    "year": "'"${YEAR}"'",
+    "trailer_type": "'"${TRAILER_TYPE}"'",
+    "hitch_type": "'"${HITCH_TYPE}"'",
+    "length": "'"${LENGTH}"'",
+    "gvwr": "'"${GVWR}"'",
+    "axels": "'"${AXELS}"'",
+    "picture": "'"${PICTURE}"'"
+  }
+}'
 
 echo
--- curl-scripts/trailers/update.sh
+++ curl-scripts/trailers/update.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 curl "https://trailer-share.herokuapp.com/trailers/${ID}" \
-  --include \
-  --request PATCH \
-  --header "Authorization: Token token=${TOKEN}" \
-  --header "Content-Type: application/json" \
-  --data '{
-    "trailer": {
-      "make": "'"${MAKE}"'",
-      "model": "'"${MODEL}"'"
-    }
-  }'
+--include \
+--request PATCH \
+--header "Authorization: Token token=${TOKEN}" \
+--header "Content-Type: application/json" \
+--data '{
+  "trailer": {
+    "make": "'"${MAKE}"'",
+    "model": "'"${MODEL}"'",
+    "year": "'"${YEAR}"'",
+    "trailer_type": "'"${TRAILER_TYPE}"'",
+    "hitch_type": "'"${HITCH_TYPE}"'",
+    "length": "'"${LENGTH}"'",
+    "gvwr": "'"${GVWR}"'",
+    "axels": "'"${AXELS}"'",
+    "picture": "'"${PICTURE}"'"
+  }
+}'
 
 echo
